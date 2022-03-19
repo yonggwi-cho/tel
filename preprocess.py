6d36a5f5f03de4e29494018dc1adb432042a3759
Yong-Gwi Cho <yonggwi.cho@gmail.com>
2022-03-19 14:59:19 +0900

    first commit.

---
+++ preprocess.py
@@ -0,0 +1,12 @@
+import numpy as np
+import codecs
+
+class PrePocessor():
+    def __init__(self,n_sample,batch,mode):
+        self.n_sample =
+        self.batch =
+        self.mode =
+
+    def read_file(self):
+
+    def pre_process(self):
